88602705f0feb2ad1f99b6c1b44b045c89f96d82
21120450 <thehien08112003@gmail.com>
2023-11-23 07:37:58 -0800

    Add ascii Program

---
+++ nachos-3.4/code/test/ascii.c
@@ -0,0 +1,21 @@
+#include "syscall.h"
+
+int main() {
+    unsigned char c = (unsigned char)(0);
+
+    while (c <= (unsigned char)(255)) {
+        PrintInt((int)c);
+        PrintString(": ");
+        PrintChar(c);
+
+        PrintChar('\n');
+
+        if (c == (unsigned char)255) {
+            break;
+        }
+
+        c++;
+    }
+
+    Halt();
+}
