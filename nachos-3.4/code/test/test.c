87d97c461ccd83f52c2b8d526887ac07383f77be
21120412 <giabach9102@gmail.com>
2023-12-08 04:46:31 -0800

    Test file syscall program

--- nachos-3.4/code/test/test.c
+++ nachos-3.4/code/test/test.c
@@ -3,7 +3,19 @@
 
 int main() {
 	int n;
-	n = ReadInt();
-	PrintInt(n);
+	CreateFile("testCreateFile.txt");	
+	n = Open("testCreateFile.txt", 0);
+	if (n != -1) {
+		PrintString("Mo file thanh cong!\n");
+	} else {
+		PrintString("Mo file khong thanh cong!\n");
+	}
+	
+	n = Close(n);
+	if (n != -1) {
+		PrintString("Dong file thanh cong!\n");
+	} else {
+		PrintString("Dong file khong thanh cong!\n");
+	}
 	Halt();
 }
