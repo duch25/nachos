c16e36a528e0b46a44366a941b57f70884e5c59a
21120412 <giabach9102@gmail.com>
2023-12-08 04:38:57 -0800

    Add Open file syscall

--- nachos-3.4/code/filesys/filesys.cc
+++ nachos-3.4/code/filesys/filesys.cc
@@ -140,6 +140,15 @@ FileSystem::FileSystem(bool format)
         freeMapFile = new OpenFile(FreeMapSector);
         directoryFile = new OpenFile(DirectorySector);
     }
+    openf = new OpenFile*[10];
+    index = 0;
+    for (int i = 0; i < 10; ++i) {
+		openf[i] = NULL;
+	}
+	openf[index++] = this->Open("stdin", 2);
+	openf[index++] = this->Open("stdout", 3);
+	this->Create("stdin", 0);
+	this->Create("stdout", 0);
 }
 
 //----------------------------------------------------------------------
@@ -237,7 +246,38 @@ FileSystem::Open(char *name)
     if (sector >= 0) 		
 	openFile = new OpenFile(sector);	// name was found in directory 
     delete directory;
-    return openFile;				// return NULL if not found
+    index++;
+    return openf[index - 1];				// return NULL if not found
+}
+
+OpenFile* FileSystem::Open(char *name, int type)
+{
+	printf("Go here\n");
+	int freeSlot = this->FindFreeSlot();
+	Directory *directory = new Directory(NumDirEntries);
+	OpenFile *openFile = NULL;
+	int sector;
+
+	directory->FetchFrom(directoryFile);
+	sector = directory->Find(name);
+	if (sector >= 0)
+		openf[freeSlot] = new OpenFile(sector, type);	// name was found in directory 
+	delete directory;
+	if (openf[freeSlot] == NULL) {
+		printf("File Sys null\n");
+	} else {
+		printf("File sys ok\n");
+	}
+	return openf[freeSlot];				// return NULL if not found
+}
+
+int FileSystem::FindFreeSlot()
+{
+	for(int i = 2; i < 10; i++)
+	{
+		if(openf[i] == NULL) return i;		
+	}
+	return -1;
 }
 
 //----------------------------------------------------------------------
