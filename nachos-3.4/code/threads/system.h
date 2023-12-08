c7839d7311bcae3597c844e2818439d99c52531a
21120412 <giabach9102@gmail.com>
2023-12-08 04:54:58 -0800

    Implement multi processes

--- nachos-3.4/code/threads/system.h
+++ nachos-3.4/code/threads/system.h
@@ -15,6 +15,7 @@
 #include "interrupt.h"
 #include "stats.h"
 #include "timer.h"
+#include "bitmap.h"
 
 // Initialization and cleanup routines
 extern void Initialize(int argc, char **argv); 	// Initialization,
@@ -32,9 +33,13 @@ extern Timer *timer;				// the hardware alarm clock
 #ifdef USER_PROGRAM
 #include "machine.h"
 #include "synchcons.h"
+#include "synch.h"
 
 extern Machine* machine;	// user program memory and registers
 extern SynchConsole* synchConsole;
+
+extern Semaphore* addrLock;
+extern BitMap* gPhysPageBitMap;
 #endif
 
 #ifdef FILESYS_NEEDED 		// FILESYS or FILESYS_STUB 
